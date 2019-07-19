6aebf6d8423c9c3c1a371cfee1a04c3b35b443bd
paulQR <paulqr10@gmail.com>
2019-07-19 14:43:28 +0200

    1 file modified

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,11 +1,17 @@
 import{Component, OnInit} from '@angular/core';
+import{ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html'
 })
 export class ArtistaComponent implements OnInit {
-    constructor(){}
+
+    constructor(private router: ActivatedRoute){
+        this.router.params.subscribe( params =>{
+              console.log(params['id']);
+        });
+    }
 
     ngOnInit(){
 
