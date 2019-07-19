bdf7300dedf90411005196764e1d32fb0a8e2b8c
paulQR <paulqr10@gmail.com>
2019-07-19 12:31:22 +0200

    3 files modified

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -5,7 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NoimagePipe implements PipeTransform{
 
-  transform(value: any, args?:any): any{
-    return null;
+  transform( images: any[] ): string{
+    if( !images ){
+        return 'assets/img/noimage.png';
+    }
+
+    if( images.length > 0){
+        return images[0].url;
+    }else{
+        return 'assets/img/noimage.png';
+    }
   }
 }
