807626467282b3f9555cf0ba0444f2cdeb439fec
paulQR <paulqr10@gmail.com>
2019-07-22 12:52:58 +0200

    2 files modified

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -9,7 +9,11 @@ import{SpotifyService} from '../../services/spotify.service';
 })
 export class ArtistaComponent{
     artista: any = {};
+    loadingArtist: boolean;
+
     constructor(private router: ActivatedRoute, private spotify: SpotifyService ){
+
+        this.loadingArtist = true;
         this.router.params.subscribe( params =>{
               //console.log(params['id']);
               this.getArtista(params['id']);
@@ -17,9 +21,11 @@ export class ArtistaComponent{
     }
 
      getArtista(id: string){
+        this.loadingArtist = true;
         this.spotify.getArtista( id ).subscribe( artista => {
               console.log(artista);
               this.artista = artista;
+              this.loadingArtist = false;
         })
      }  
 }
