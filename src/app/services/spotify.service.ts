2f5d56e6904c45a3dc215ab67aa741a49d86ca86
paulQR <paulqr10@gmail.com>
2019-07-22 11:52:59 +0200

    1 file modified

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -66,10 +66,11 @@ export class SpotifyService{
 
     }
     */
-    getArtista(id: string){
+    getArtista(termino: string){
         //return this.getQuery(`artists/${ id }`).pipe( map( data => data['artists'].items));
 
-        return this.getQuery(`artists/${ id }`);
+        //return this.getQuery(`artists/${ id }`);
 
+        return this.getQuery(`search?q=${ termino }&type=artist&limit=15`).pipe( map( data => data['artists'].items));;
     }    
 }
